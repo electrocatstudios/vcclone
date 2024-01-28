b279a5960b223c047beb6d982e3ac8ddd2e71374
filtoid <philjeffes@hotmail.com>
2024-01-28 21:28:55 +0100

    show the stored latest message

--- src/pages/game.rs
+++ src/pages/game.rs
@@ -3,11 +3,19 @@ use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, window};
 use web_sys::wasm_bindgen::{JsCast, JsValue};
 use web_sys::wasm_bindgen::prelude::Closure;
 
+use gloo_console::log;
+
 pub enum GameMsg {
+    MouseDown((f64,f64)),
+    MouseUp((f64,f64)),
+    MouseMove((f64,f64)),
     Render
 }
 
 pub struct GameControl {
+    last_x: f64,
+    last_y: f64,
+    last_action: String,
     canvas: NodeRef,
     callback: Closure<dyn FnMut()>,
 }
@@ -23,6 +31,9 @@ impl Component for GameControl {
         ctx.link().send_message(GameMsg::Render);
 
         Self{
+            last_x: 0.0,
+            last_y: 0.0,
+            last_action: "".to_string(),
             canvas: NodeRef::default(),
             callback: callback
         }
@@ -30,6 +41,26 @@ impl Component for GameControl {
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
+            GameMsg::MouseDown(evt) => {
+                log!("Mouse down ", evt.0, evt.1);
+                self.last_x = evt.0;
+                self.last_y = evt.1;
+                self.last_action = "Mouse down".to_string();
+                true
+            },
+            GameMsg::MouseUp(evt) => {
+                log!("Mouse up ", evt.0, evt.1);
+                self.last_x = evt.0;
+                self.last_y = evt.1;
+                self.last_action = "Mouse Up".to_string();
+                true
+            },
+            GameMsg::MouseMove(evt) => {
+                self.last_x = evt.0;
+                self.last_y = evt.1;
+                self.last_action = "Mouse Move".to_string();
+                true
+            },
             GameMsg::Render => {
                 self.render();
                 true
@@ -37,12 +68,27 @@ impl Component for GameControl {
         }
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let onmousedown = ctx.link().callback(move |evt:MouseEvent| {
+            GameMsg::MouseDown(( evt.page_x() as f64, evt.page_y() as f64) )
+        });
+        let onmouseup = ctx.link().callback(move |evt:MouseEvent| {
+            GameMsg::MouseUp(( evt.page_x() as f64, evt.page_y() as f64) )
+        });
+        let onmousemove = ctx.link().callback(move |evt:MouseEvent| {
+            GameMsg::MouseMove(( evt.page_x() as f64, evt.page_y() as f64) )
+        });
+        
+
         html! {
             <div class="game_canvas">
                 <canvas id="canvas"
                     style={"margin: 0px; width: 100vw; height: 90vh; left: 0px; top:0px;"}
-                    ref={self.canvas.clone()}>
+                    ref={self.canvas.clone()}
+                    onmousedown={onmousedown}
+                    onmouseup={onmouseup}
+                    onmousemove={onmousemove}
+                    >
                 </canvas>
             </div>
         }
@@ -68,6 +114,13 @@ impl GameControl {
         ctx.move_to(50.0, 50.0);
         ctx.line_to(200.0, 200.0);
         ctx.stroke();
+
+        ctx.set_fill_style(&JsValue::from("rgb(255,0,0)"));
+        ctx.set_font("48px serif");
+        let _ = ctx.fill_text(self.last_action.as_str(), 300.0, 300.0);
+
+        let loc_string = "X: ".to_owned() + self.last_x.to_string().as_str() + ", Y: " + self.last_y.to_string().as_str();
+        let _ = ctx.fill_text(loc_string.as_str(), 300.0, 350.0);
 
         window()
             .unwrap()
